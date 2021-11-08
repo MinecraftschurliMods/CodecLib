939cfe54c4e7fe636dfd4298bf60c10f69f42136
Minecraftschurli <minecraftschurli@gmail.com>
2021-11-08 09:53:26 +0100

    add more functionality

--- src/main/java/com/github/minecraftschurli/codeclib/CodecDataManager.java
+++ src/main/java/com/github/minecraftschurli/codeclib/CodecDataManager.java
@@ -18,12 +18,13 @@ import net.minecraftforge.event.OnDatapackSyncEvent;
 import net.minecraftforge.fmllegacy.network.NetworkDirection;
 import net.minecraftforge.fmllegacy.network.NetworkEvent;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.*;
 import java.util.function.Supplier;
 
-public class CodecDataManager<T> extends SimpleJsonResourceReloadListener {
+public class CodecDataManager<T> extends SimpleJsonResourceReloadListener implements Map<ResourceLocation, T> {
     private static final BiMap<Integer, CodecDataManager<?>> DATA_MANAGER = HashBiMap.create();
     private static final Gson GSON = new Gson();
     private final String folderName;
@@ -58,6 +59,78 @@ public class CodecDataManager<T> extends SimpleJsonResourceReloadListener {
     }
 
     @Override
+    public int size() {
+        return this.data != null ? this.data.size() : 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.data != null && this.data.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return this.data != null && this.data.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return this.data != null && this.data.containsValue(value);
+    }
+
+    @Nullable
+    @Override
+    public T get(@Nullable Object id) {
+        if (id == null) return null;
+        if (id instanceof String keyS) {
+            id = new ResourceLocation(keyS);
+        }
+        if (!(id instanceof ResourceLocation)) throw this.logger.throwing(new ClassCastException());
+        if (this.data == null) return null;
+        return this.data.get(id);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    public T getOrThrow(@Nullable Object id) {
+        if (this.data == null) throw this.logger.throwing(new IllegalStateException(
+                "CodecDataManager(%s) not loaded yet!".formatted(this.folderName)));
+        if (!this.containsKey(id) || id == null) throw this.logger.throwing(new NoSuchElementException());
+        return this.get(id);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    public T getOrDefault(@Nullable Object id, Supplier<T> defaultSupplier) {
+        if (this.data == null || !this.containsKey(id) || id == null) return defaultSupplier.get();
+        return this.get(id);
+    }
+
+    public Optional<T> getOptional(@Nullable Object id) {
+        return Optional.ofNullable(this.get(id));
+    }
+
+    public Map<ResourceLocation, T> getData() {
+        return Collections.unmodifiableMap(this.data);
+    }
+
+    @NotNull
+    @Override
+    public Set<ResourceLocation> keySet() {
+        return Collections.unmodifiableSet(this.data.keySet());
+    }
+
+    @NotNull
+    @Override
+    public Collection<T> values() {
+        return Collections.unmodifiableCollection(this.data.values());
+    }
+
+    @NotNull
+    @Override
+    public Set<Entry<ResourceLocation, T>> entrySet() {
+        return Collections.unmodifiableSet(this.data.entrySet());
+    }
+
+    @Override
     protected final void apply(Map<ResourceLocation, JsonElement> dataIn, ResourceManager resourceManager, ProfilerFiller profiler) {
         this.logger.info("Beginning loading of data for data loader: {}", this.folderName);
         profiler.push("data_manager_%s_deserialize".formatted(this.folderName));
@@ -67,7 +140,7 @@ public class CodecDataManager<T> extends SimpleJsonResourceReloadListener {
         this.logger.info("Beginning validation of data for data loader: {}", this.folderName);
         profiler.push("data_manager_%s_validate".formatted(this.folderName));
         try {
-            this.validator.validate(this.data, logger);
+            this.validator.validate(this.data, logger); // yes it is intentional to pass the mutable data
             this.logger.info("Data loader for {} finished validation of {} entries", this.folderName, this.data.size());
         } catch (ValidationError e) {
             this.logger.error("Data loader for {} failed validation", this.folderName, e);
@@ -87,44 +160,8 @@ public class CodecDataManager<T> extends SimpleJsonResourceReloadListener {
 
     public final CodecDataManager<T> subscribeAsSyncable(NetworkHandler networkHandler) {
         networkHandler.register(SyncPacket.class, NetworkDirection.PLAY_TO_CLIENT);
-        MinecraftForge.EVENT_BUS.addListener((OnDatapackSyncEvent event) -> networkHandler.sendToPlayerOrAll(
-                new SyncPacket<>(DATA_MANAGER.inverse().get(this), this.data), event.getPlayer()
-        ));
+        MinecraftForge.EVENT_BUS.addListener((OnDatapackSyncEvent event) -> networkHandler.sendToPlayerOrAll(new SyncPacket<>(DATA_MANAGER.inverse().get(this), this.data), event.getPlayer()));
         return this;
-    }
-
-    public T get(@Nullable ResourceLocation id) {
-        if (this.data == null) {
-            throw this.logger.throwing(new IllegalStateException("CodecDataManager(%s) not loaded yet!".formatted(this.folderName)));
-        }
-        return this.getOptional(id).orElseThrow();
-    }
-
-    @Nullable
-    public T getNullable(@Nullable ResourceLocation id) {
-        if (id == null) return null;
-        if (this.data == null) return null;
-        return this.data.get(id);
-    }
-
-    public T getOrDefault(@Nullable ResourceLocation id, Supplier<T> defaultSupplier) {
-        return this.getOptional(id).orElseGet(defaultSupplier);
-    }
-
-    public Optional<T> getOptional(@Nullable ResourceLocation id) {
-        return Optional.ofNullable(this.getNullable(id));
-    }
-
-    public Map<ResourceLocation, T> getData() {
-        return Collections.unmodifiableMap(this.data);
-    }
-
-    public Set<ResourceLocation> getKeys() {
-        return Collections.unmodifiableSet(this.data.keySet());
-    }
-
-    public Collection<T> getValues() {
-        return Collections.unmodifiableCollection(this.data.values());
     }
 
     public static final class SyncPacket<T> implements IPacket {
@@ -165,4 +202,27 @@ public class CodecDataManager<T> extends SimpleJsonResourceReloadListener {
     public interface Validator<T> {
         void validate(T data, Logger logger) throws ValidationError;
     }
+
+    // region unsupported operations
+    @Override
+    public T put(ResourceLocation key, T value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T remove(Object key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void putAll(@NotNull final Map<? extends ResourceLocation, ? extends T> m) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    // endregion
 }
